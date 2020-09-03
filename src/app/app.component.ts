1739e5ee498af4486d5807defe8580b8c78a617e
Varun Srinivas <varun1505@gmail.com>
2020-09-03 15:42:31 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bkst-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bookstagram';
+}
