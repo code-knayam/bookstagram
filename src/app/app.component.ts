5642e35564f5d54ba342dd021e26ef6d1ff9c820
Varun Srinivas <varun@coditas.com>
2020-09-04 12:41:45 +0530

    added: prettier config options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'bkst-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'bkst-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bookstagram';
+	title = 'bookstagram'
 }
