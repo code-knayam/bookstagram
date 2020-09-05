c5984627471a70ccfed14e35c173e3e53c716d11
varun-coditas <varun@coditas.com>
2020-09-05 13:43:14 +0530

    ✨ Login page UI done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, HeaderComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
 		path: 'register',
 		component: RegisterComponent,
 	},
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'login',
+	},
 ]
 
 @NgModule({
